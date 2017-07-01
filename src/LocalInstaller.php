b426cb6da0724919098aceac7aed15799f5da699
gossi <gossi@users.noreply.github.com>
2017-07-01 11:54:32 +0200

    php 5.3 compatible

--- src/LocalInstaller.php
+++ src/LocalInstaller.php
@@ -2,74 +2,74 @@
 namespace gossi\composer\localdev;
 
 use Composer\Composer;
+use Composer\IO\IOInterface;
 use Composer\Installer\InstallationManager;
 use Composer\Installer\InstallerInterface;
-use Composer\IO\IOInterface;
 use Composer\Package\PackageInterface;
 use Composer\Repository\InstalledRepositoryInterface;
 use Composer\Util\Filesystem;
 
 class LocalInstaller implements InstallerInterface {
-	
+
 	private $composer;
 	private $io;
 	private $repo;
 	private $filesystem;
 	private $installManager;
-	
+
 	public function __construct(Composer $composer, IOInterface $io, LocalRepository $repo) {
 		$this->composer = $composer;
 		$this->io = $io;
 		$this->repo = $repo;
 		$this->filesystem = new Filesystem();
 	}
-	
+
 	public function getInstallerManager() {
 		return $this->installManager;
 	}
-	
+
 	public function setInstallManager(InstallationManager $installManager) {
 		$this->installManager = $installManager;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param PackageInterface $package
 	 * @return InstallerInterface
 	 */
 	private function getDedicatedInstaller(PackageInterface $package) {
 		return $this->getInstallerManager()->getInstaller($package->getType());
 	}
-	
+
 	private function handlePackage(PackageInterface $package) {
 		$packageId = $package->getName();
-		
+
 		foreach ($this->repo->getPackages() as $repoPackage) {
 			if ($packageId === $repoPackage->getName()) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	private function ensureSymlink(PackageInterface $package) {
 		$installPath = $this->getDedicatedInstaller($package)->getInstallPath($package);
 		$originPath = $this->repo->getPath($package->getName());
-		
+
 		// return if link is already well placed
 		if (is_link($originPath) && readlink($originPath) == $installPath) {
 			return;
 		}
-		
+
 		// remove installation first...
 		if (file_exists($installPath)) {
 			$this->filesystem->removeDirectory($installPath);
 		}
-		
+
 		// ... then create a symlink
 		$this->symlink($originPath, $installPath);
-		
+
 		$this->io->write(sprintf('    => Symlinked <info>%s</info> from <fg=magenta>%s</>', $package->getName(), $originPath), true);
 		$this->io->write('', true);
 	}
@@ -87,15 +87,15 @@ class LocalInstaller implements InstallerInterface {
 	private function symlink($originDir, $targetDir) {
 		// Windows logic
 		if (defined('PHP_WINDOWS_VERSION_MAJOR')) {
-			$output = [];
+			$output = array();
 			$return = 0;
 			exec('mklink /J ' . escapeshellarg($targetDir) . ' ' . escapeshellarg($originDir), $output, $return);
-			
+
 			if ($return === 0) {
 				return;
 			}
 		}
-		
+
 		@mkdir(dirname($targetDir), 0777, true);
 
 		$ok = false;
@@ -122,7 +122,7 @@ class LocalInstaller implements InstallerInterface {
 
 	public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package) {
 		$installPath = $this->getInstallPath($package);
-		
+
 		// remove symlink, if package is handled and symlinked
 		if ($this->handlePackage($package) && is_link($installPath)) {
 			unlink($installPath);
@@ -139,7 +139,7 @@ class LocalInstaller implements InstallerInterface {
 // 		printf("Handle Install: %s\n", $package->getName());
 		// if this packages is handled but already symlinked, circumvent installation to suppress
 		// modified packaged warning (ask for discard)
-		if (!($this->handlePackage($package) && is_link($this->getInstallPath($package)))) { 
+		if (!($this->handlePackage($package) && is_link($this->getInstallPath($package)))) {
 			$installer = $this->getDedicatedInstaller($package);
 			$installer->install($repo, $package);
 		}
